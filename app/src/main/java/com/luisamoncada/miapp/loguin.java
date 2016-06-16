19c31f69686557caf2b5b82d35dac18c6f4e20ed
lumoncada <lumoncada9@gmail.com>
2016-06-16 01:55:22 -0500

    Login facebook adelantado

--- app/src/main/java/com/luisamoncada/miapp/loguin.java
+++ app/src/main/java/com/luisamoncada/miapp/loguin.java
@@ -1,5 +1,6 @@
 package com.luisamoncada.miapp;
 
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.pm.Signature;
@@ -7,13 +8,25 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 
 public class loguin extends AppCompatActivity {
+
+CallbackManager callbackManager;
+TextView name, mail;
+    ImageView foto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,8 +34,42 @@ public class loguin extends AppCompatActivity {
         FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_loguin);
 
-        generarKeyHashes();
+        name = (TextView) findViewById(R.id.tName);
+        mail = (TextView) findViewById(R.id.tMail);
+        foto = (ImageView) findViewById(R.id.iFoto);
+
+        //generarKeyHashes();
+        LoginButton loginButton = (LoginButton) findViewById(R.id.login_button);
+        callbackManager =CallbackManager.Factory.create();
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Profile perfil = com.facebook.Profile.getCurrentProfile();
+                name.setText(perfil.getLastName());
+                mail.setText(perfil.getLastName());
+
+            }
+
+            @Override
+            public void onCancel() {
+                name.setText("Se cancelo el Login");
+
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                name.setText("Error de login:"+ error);
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
     public void generarKeyHashes(){
         try {
             PackageInfo info = getPackageManager().getPackageInfo(
